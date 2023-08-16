d46c57fca7ef2fff2160f312af7ead50af0e97e5
professorisidro <isidro@professorisidro.com.br>
2023-08-16 10:12:07 -0300

    First Commit

---
+++ src/main/java/br/com/isiflix/exemplocicd/ExemplocicdApplication.java
@@ -0,0 +1,13 @@
+package br.com.isiflix.exemplocicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplocicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplocicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isiflix/exemplocicd/ExemplocicdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isiflix.exemplocicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplocicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
